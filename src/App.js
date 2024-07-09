0abc656d3a4d12c910d2bf3c798c60a113e84f5e
BalaKrishnaBaddii <bkrishna1114@gmail.com>
2024-07-09 12:34:05 +0530

    basic structure is ready

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import "./app.css";
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Calculator />
+    </>
   );
 }
 
-export default App;
+const Calculator = () => (
+  <div className="calculator">
+    <div className="upper">
+      <Amount />
+      <Currency />
+      <Currency />
+    </div>
+    <Output />
+  </div>
+);
+
+const Amount = () => {
+  return (
+    <div className="amount">
+      <input type="number" />
+    </div>
+  );
+};
+
+const Currency = () => {
+  return (
+    <select className="currency">
+      <option>USD</option>
+      <option>EUR</option>
+      <option>INR</option>
+      <option>CAD</option>
+    </select>
+  );
+};
+
+const Output = () => {
+  return <label className="output">10.5</label>;
+};
